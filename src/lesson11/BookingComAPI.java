a44811639a90f4c46332a4045a0b73f9b9cbad57
toniname <60040991+toniname@users.noreply.github.com>
2020-05-16 17:30:55 +0300

    Update BookingComAPI.java

--- src/lesson11/BookingComAPI.java
+++ src/lesson11/BookingComAPI.java
@@ -9,60 +9,50 @@ import lesson9.l2.User;
 // BookingComAPI вернет все комнаты с ценой в диапазоне 0 - 150
 //------------------------------------------------------------------------------
 public class BookingComAPI implements API {
-    Room[] rooms;
-
-    public BookingComAPI(Room[] rooms) {
-        this.rooms = rooms;
-    }
-
-    @Override
-    //сначала продишь циклом и считаешь сколько комнат найдет
-    //потом создаешь массив нужно длины. и потом еще один цикл - заполнить этот массив комнатами
-    public Room[] findRooms(int price, int persons, String city, String hotel) {
-        int maxPrice = price + 100;
-        int minPrice = price - 100;
-        int x = 0;
-        Room[] rooms2 = new Room[countRooms(price, persons, city, hotel)];
-        for (Room room : rooms) {
-            if (room != null && (room.getPrice() <= maxPrice && room.getPrice() >= minPrice) && room.getPersons() == persons &&
-                    room.getCityName().equals(city) && room.getHotelName().equals(hotel)) {
-                rooms2[x] = room;
-            }
-
-        }
-        return rooms2;
-
-    }
-
-    // ---------------------------------------------------------------------------------------------------------------------
-    public int countRooms(int price, int persons, String city, String hotel) {
-        int i = 0;
-        int maxPrice = price + 100;
-        int minPrice = price - 100;
-        for (Room room : rooms) {
-            if (room != null && (room.getPrice() <= maxPrice && room.getPrice() >= minPrice) &&
-                    room.getPersons() == persons && room.getCityName().equals(city) && room.getHotelName().equals(hotel))
-                i++;
-        }
-        return i;
-    }
-
-    //------------------------------------------------------------------------------------------------------------------------
-    @Override
-    public Room[] getAll() {
-        int i = 0;
-        for (Room room : rooms) {
-            if (room != null)
-                i++;
-        }
-        Room[] rooms1 = new Room[i];
-        int x = 0;
-        for (Room room : rooms) {
-            if (room != null) {
-                rooms1[x] = room;
-            }
-
-        }
-        return rooms1;
-    }
+Room[] rooms;
+public BookingComAPI(Room[] rooms) {
+this.rooms = rooms;
+}
+@Override
+//сначала продишь циклом и считаешь сколько комнат найдет
+//потом создаешь массив нужно длины. и потом еще один цикл - заполнить этот массив комнатами
+public Room[] findRooms(int price, int persons, String city, String hotel) {
+int i = 0;
+int maxPrice = price + 100;
+int minPrice = price - 100;
+for (Room room : rooms) {
+if (room != null && room.getPrice() <= maxPrice && room.getPrice() >= minPrice &&
+room.getPersons() == persons && room.getCityName().equals(city) && room.getHotelName().equals(hotel)) {
+i++;
+}
+}
+int x = 0;
+Room[] res = new Room[i];
+for (Room room : rooms) {
+if (room != null && room.getPrice() <= maxPrice && room.getPrice() >= minPrice && room.getPersons() == persons &&
+room.getCityName().equals(city) && room.getHotelName().equals(hotel)) {
+res[x] = room;
+x++;
+}
+}
+return res;
+}
+@Override
+public Room[] getAll() {
+int i = 0;
+for (Room room : rooms) {
+if (room != null) {
+i++;
+}
+}
+Room[] res = new Room[i];
+int x = 0;
+for (Room room : rooms) {
+if (room != null) {
+res[x] = room;
+x++;
+}
+}
+return res;
+}
 }
