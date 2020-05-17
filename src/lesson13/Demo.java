cf0ff8aba80eaf5ead972761d841490c19fbb829
toniname <drednoyta1@ukr.net>
2020-05-17 08:08:54 +0300

    ordered

--- src/lesson13/Demo.java
+++ src/lesson13/Demo.java
@@ -6,19 +6,19 @@ import java.util.Arrays;
 public class Demo {
     public static void main(String[] args) {
         UserRepository userRepository = new UserRepository();
-
-        System.out.println(Arrays.deepToString(userRepository.getUsers()));
-
-        User user = new User(232324, "Ann", "dfg1ff3g455");//1 сохранение юзера
-        userRepository.save(user);
-        System.out.println(Arrays.deepToString(userRepository.getUsers()));
-
-        userRepository.save(user);//2 сохранение того же юзера
-        System.out.println(Arrays.deepToString(userRepository.getUsers()));//2 сохранение того же юзера
         //1 сохранение юзера
         //2 сохранение того же юзера
         //3 когда нет места в массиве
         //4  сохраняем нал
+        System.out.println(Arrays.deepToString(userRepository.getUsers()));
+
+        User user = new User(232324, "Ann", "dfg1ff3g455");//1 сохранение юзера
+         userRepository.save(user);
+        System.out.println(Arrays.deepToString(userRepository.getUsers()));
+
+        userRepository.save(user);//2 сохранение того же юзера
+        System.out.println(Arrays.deepToString(userRepository.getUsers()));//2 сохранение того же юзера
+
         int i = 15;
         while (i > 0) {
             User user1 = new User(i, "Ann", "dfg1ff3g455");//3 когда нет места в массиве
@@ -30,12 +30,13 @@ public class Demo {
         userRepository.save(null); //4  сохраняем нал
 
         //test update
-        user = new User(232324, "Ann", "dfg1ff");//1 обновление юзера
-        userRepository.update(user);
-        System.out.println(Arrays.deepToString(userRepository.getUsers()));
         //1 обновление юзера
         //2 когда нет юзера на обновление
         //3  обновляем нал
+        user = new User(232324, "Ann", "dfg1ff");//1 обновление юзера
+        userRepository.update(user);
+        System.out.println(Arrays.deepToString(userRepository.getUsers()));
+
         user = new User(232323, "Ann", "dfg1ff");//2 когда нет юзера на обновление
         System.out.println(userRepository.update(user));
         System.out.println(Arrays.deepToString(userRepository.getUsers()));
