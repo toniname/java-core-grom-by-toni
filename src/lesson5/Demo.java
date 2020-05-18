22b9f629487cb00ac5a00ce433b9a4ea6401ee82
toniname <drednoyta1@ukr.net>
2020-05-18 11:35:52 +0300

    homework

--- src/lesson5/Demo.java
+++ src/lesson5/Demo.java
@@ -1,12 +1,30 @@
 package lesson5;
 
+import java.util.Arrays;
+
 public class Demo {
     public static void main(String[] args) {
+
+        //для ArraysPractice
         ArraysPractice arraysPractice = new ArraysPractice();
+        int[] array = {-10, 0, 110, 555, 1000, -45, 0, 60};
+        //1 просто вывод элементов
+        System.out.println(ArraysPractice.maxElement(array));
+        //2 когда все элементы равны
+        int[] array1 = {10, 10, 10, 10, 10, 10, 10, 10};
+        System.out.println(ArraysPractice.maxElement(array1));
+        //3 когда все элементы 0
+        int[] array2 = {0, 0, 0, 0, 0, 0, 0, 0};
+        System.out.println(ArraysPractice.maxElement(array2));
 
-
-
-
+        // для BanksPractice
+        BanksPractice banksPractice = new BanksPractice();
+        //просто вывод данных
+        String[] names = {"Jack", "Ann", "Denis", "Andrey", "Nicolay", "Irina", "John"};
+        int[] balances = {100, 500, 8432, -99, 12000, -54, 0};
+        System.out.println(Arrays.toString( BanksPractice.findClientsByBalance(names, balances, 100)));
+        System.out.println(Arrays.toString(BanksPractice.findClientsWithNegativeBalance(names, balances)));
+        
 
         System.out.println();
     }
