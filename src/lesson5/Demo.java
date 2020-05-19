732504d33251ffdd02114631d3d19c5310bd0ade
toniname <drednoyta1@ukr.net>
2020-05-19 12:38:30 +0300

    homework

--- src/lesson5/Demo.java
+++ src/lesson5/Demo.java
@@ -24,8 +24,30 @@ public class Demo {
         int[] balances = {100, 500, 8432, -99, 12000, -54, 0};
         System.out.println(Arrays.toString( BanksPractice.findClientsByBalance(names, balances, 100)));
         System.out.println(Arrays.toString(BanksPractice.findClientsWithNegativeBalance(names, balances)));
-        
+        //2 балансы совпадают
 
-        System.out.println();
+        // для finde5unique
+        //просто вывод данных
+        finde5unique finde5unique = new finde5unique();
+        int[] array3 = {11, 12, 10, 5, 4, 44, 100, 44, 10, 11, 3};
+        System.out.println(lesson5.finde5unique.uniqueCount(array3));
+        //2 когда все элементы равны
+        int[] array4 = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+        System.out.println(lesson5.finde5unique.uniqueCount(array4));
+
+        // для owerDraft
+        owerDraft owerDraft = new owerDraft();
+        //просто вывод данных
+        String[] clients = {"Jack", "Ann", "Denis", "Andrey", "Nicolay", "Irina", "John"};
+        int[] balances1 = {100, 500, 8432, -99, 12000, -54, 0};
+        System.out.println(owerDraft.withdraw(clients, balances1, "Denis", 8432));
+        //2 когда amount = balances
+        String[] clients1 = {"Jack", "Ann", "Denis", "Andrey", "Nicolay", "Irina", "John"};
+        int[] balances2 = {100, 500, 8432, -99, 12000, -54, 0};
+        System.out.println(owerDraft.withdraw(clients1, balances2, "Denis", 8432));
+        //3 когда клиент с отрицательным балансом
+        String[] clients2 = {"Jack", "Ann", "Denis", "Andrey", "Nicolay", "Irina", "John"};
+        int[] balances3 = {100, 500, 8432, -99, 12000, -54, 0};
+        System.out.println(owerDraft.withdraw(clients2, balances3, "Andrey", 50));
     }
 }
