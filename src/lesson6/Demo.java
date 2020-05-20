4589ef423460d7f9edbe002d0fe349d70934e282
toniname <drednoyta1@ukr.net>
2020-05-20 22:59:27 +0300

    homework

--- src/lesson6/Demo.java
+++ src/lesson6/Demo.java
@@ -1,20 +1,39 @@
 package lesson6;
-import lesson6.Car;
+
+
+import java.util.Arrays;
+import java.util.Date;
+
 public class Demo {
     public static void main(String[] args) {
+        //проверка Сar
         Car carObject = new Car(10000, 2015, "Anton");
         Car car = new Car(3, 4, "dsfds");
         System.out.println(carObject.color);
         System.out.println(carObject.horsePower);
         System.out.println(carObject.ownerName);
         carObject.horsePower = 100;
-
         System.out.println(carObject.horsePower);
-
         carObject.startRun();
         carObject.stopRun();
         carObject.changeOwner("Tast");
-
         System.out.println(carObject.ownerName);
+        //проверка DemoHomeWork
+        DemoHomeWork demoHomeWork = new DemoHomeWork();
+        System.out.println(demoHomeWork.createOrderAndCallMethods());
+        System.out.println(demoHomeWork.createOrder());
+        //проверка Order
+        Order order = new Order();
+        System.out.println(order.checkPrice());
+        System.out.println(order.isValidType());
+        //проверка User
+        User user = new User("dfdfsf", 45, "Kiev", new Date(), true );
+        System.out.println(user.name);
+        System.out.println(user.age);
+        System.out.println(user.city);
+        System.out.println(user.LastActiveDate);
+        System.out.println(user.isActive);
+
+
     }
 }
