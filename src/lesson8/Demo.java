a7dd32dd9b12c08f9e444d9d16d368fd92fa27e6
toniname <drednoyta1@ukr.net>
2020-06-01 10:15:58 +0300

    for homework

--- src/lesson8/Demo.java
+++ src/lesson8/Demo.java
@@ -2,14 +2,21 @@ package lesson8;
 
 public class Demo  {
     public static void main(String[] args) {
+        //проверка SavingAccount
         SavingAccount savingAccount = new SavingAccount("SomeBank", "Denis", 1000, 50 );
         savingAccount.depositMoney(100);
         System.out.println(savingAccount.moneyAmount);
-
         savingAccount.changeOwnerName("Oleg");
         System.out.println(savingAccount.ownerName);//достает поле ownerName из класса SavingAccount через метод savingAccount
+
+        //проверка CheckingAccount
         CheckingAccount checkingAccount = new CheckingAccount("SomeBank", "Denis", 1000, 500 );
         checkingAccount.changeOwnerName("Oleg");
         System.out.println(checkingAccount.ownerName);
+
+        //проверка Account
+        Account account = new Account("bank", "qweqwe", 3579);
+        System.out.println(account);
+        account.depositMoney(2322411);
     }
 }
